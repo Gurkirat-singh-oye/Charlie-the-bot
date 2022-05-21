c724e63cf64a0fed662c3cc9c70e6a468e5609d9
ThatOctoberMan <saggishm@gmail.com>
2022-05-21 16:27:57 +0530

    few things

--- devhere.py
+++ devhere.py
@@ -1,11 +1,37 @@
 from lib2to3.pgen2.pgen import ParserGenerator
+from socket import J1939_PGN_ADDRESS_COMMANDED
 import requests as rq
 import random
 from requests_html import HTMLSession
+import urllib
 import lxml
+import json
 import re
+
+jsonctx = open('savedsongs.json','r+')
+songdic = json.load(jsonctx)
+
+async def findmeurl(tis):
+    html = urllib.request.urlopen(
+        f"https://www.youtube.com/results?search_query={tis}")
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+
+    url = f'https://www.youtube.com/watch?v={video_ids[0]}'
+    return url
 
 async def news():
     tmp = rq.get('https://www.thehindu.com/')
     links = (re.findall( "href=[\"\'](https://www.thehindu.com/news/.*?ece)[\"\']", tmp.text))
     return (random.choice(links))
+
+async def savethissong(song, here):
+    try:
+        songdic['savedsongs'][here].index(song)
+        return "This is already there"
+    except ValueError:
+        pass
+    songdic['savedsongs'][here].append(song)
+    jsonctx.seek(0)
+    (json.dump(songdic,jsonctx))
+    jsonctx.truncate()
+    return f"this is your list (links and names){songdic['savedsongs'][here]}"
--- kanjar.py
+++ kanjar.py
@@ -1,7 +1,11 @@
 from email import message
+from email.policy import default
+from multiprocessing.connection import Client
+from turtle import title
 import discord
 from discord.ext import commands, tasks
 import os
+import time
 import re
 import devhere
 from random import randint
@@ -13,6 +17,7 @@ import pafy
 C_token = 'NjkyOTU1OTg2MjgwODQxMzIx.GErMTU.7K7x7Ah03GscqSD2qN0BGW7FTLpyluK2Dm9x1U'
 global cnt
 cnt =0
+playerlist = []
   
 client = commands.Bot(command_prefix='`', intents = discord.Intents.all())
 
@@ -27,39 +32,68 @@ async def join(ctx):
     await ctx.author.voice.channel.connect()
 
 @client.command(name="play")
-async def play(ctx,tis):
+async def play(ctx,tis=None):
     voice_channel = ctx.author.voice.channel
-    await ctx.author.voice.channel.connect()
+    voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+    if voice_client and voice_client.is_connected():
+        pass
+    else:
+        await ctx.author.voice.channel.connect()
     print (tis)
-    html = urllib.request.urlopen(
-        f"https://www.youtube.com/results?search_query={tis}")
-    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    
+    if tis!=None:
+        url = await devhere.findmeurl(tis)
+        video = pafy.new(url)
+        wait = (int((video.duration).split(':')[0])*60*60+int((video.duration).split(':')[1])*60+int((video.duration).split(':')[2]))
+        playerlist.append(video)
+    print (playerlist)
+    try:
+        best = playerlist[0].audiostreams[0]
+        player = (discord.FFmpegPCMAudio(best.url))
+        ctx.voice_client.play(player)
+        await ctx.channel.send(embed = discord.Embed(title=playerlist[0].title,url=f"https://www.youtube.com/watch?v={playerlist[0].videoid}").set_image(url=playerlist[0].bigthumb))
+        playerlist.pop(0)
+    except discord.errors.ClientException:
+        print("still playing")
+        pass
+    
 
-    url = f'https://www.youtube.com/watch?v={video_ids[0]}'
-    print (url)
-    video = pafy.new(url)
-    best = video.audiostreams[0]
-    player = (discord.FFmpegPCMAudio(best.url))
-    ctx.voice_client.play(player)
+@client.command()
+async def skip(ctx):
+    await stop(ctx)
+    await play(ctx)
+
+@client.command()
+async def stop(ctx):
+    ctx.voice_client.stop()
 
 @client.command()
 async def news(ctx):
     newsart = await devhere.news()
     await ctx.channel.send(newsart)
 
+@client.command()
+async def save(ctx,arg1,arg2 = 'default'):
+    say = await devhere.savethissong(arg1,arg2)
+    await ctx.channel.send(say)
+
+@client.command()
+async def playserveradio(ctx):
+    for a in devhere.songdic["savedsongs"]["default"]:
+        await ctx.channel.send(f"playing these songs")
+
 @client.event
 async def on_message(message):
     print("ye chla")
     if client.user.mentioned_in(message):
             await message.channel.send("Dubara mention mat kario, gaand mardunga!!")
-            await message.channel.send(message.author)
     word_list = ['rl?', 'valo?', 'chutiya', 'ass', 'titties', 'nipple', 'Ass', 'ASS', 'bhenchod', 'gaand']
     messageContent = message.content
     if len(messageContent) > 0:
         for word in word_list:
             if word in messageContent:
                 r = urllib.request.urlopen(
-    "https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (word, "HYDM53SGJD2K", 10))
+    "https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (word, "HYDM53SGJD2K", 20))
                 #print (r)
                 r=r.read().decode()
                 #print(r)
@@ -70,7 +104,7 @@ async def on_message(message):
 ##                print (temp['results'][0]['media'][0]['mediumgif']['url'])
                 gif_s = re.findall(r"https://(\S{61}).gif", r)
                 print(len(gif_s))
-                await message.channel.send("https://" + gif_s[randint(1,10)*7 - 7] + ".gif")
+                await message.channel.send("https://" + gif_s[randint(1,20)*7 - 7] + ".gif")
     await client.process_commands(message)
     
 client.run(C_token)
